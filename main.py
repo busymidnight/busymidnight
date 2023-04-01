382236be7606c10b2c26681ae4a5414b83761614
busymidnight <100250055+busymidnight@users.noreply.github.com>
2023-04-01 18:13:16 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import feedparser,time,re
+import feedparser,time
 
 URL="http://un-lazy-midnight.tistory.com/rss"
 RSS_FEED = feedparser.parse(URL)
 MAX_NUM = 5
-latest_posts = ""
-
 markdown_text="""
 ## 💎 Latest Blog Posts
 
@@ -17,6 +15,9 @@ for idx, feed in enumerate(RSS_FEED['entries']):
   feed_date = feed['published_parsed']
   markdown_text += f"[{time.strftime('%Y/%m/%d', feed_date)} - {feed['title']}]({feed['link']}) <br/>\n"
 
-f = open("README.md", mode="w", encoding="utf-8")
-f.write(markdown_text)
-f.close()
+with open("README.md",'r+') as f:
+    content = f.read()
+    new_content =  content + markdown_text
+    f.seek(0)
+    f.write(new_content)
+    f.truncate()
