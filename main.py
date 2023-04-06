a5533f6af328ddc02b9105975850f4c0580bfe5e
busymidnight <100250055+busymidnight@users.noreply.github.com>
2023-04-06 16:58:30 +0900

    Update main.py

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ markdown_text="""
    <img src="https://img.shields.io/badge/css-1572B6?style=flat-square&logo=css3&logoColor=white"/></a>&nbsp 
    <br>
  </p>
- 
+  <br>
   <a href="https://hits.seeyoufarm.com"><img src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2Fbusymidnight&count_bg=%23BEBEBE&title_bg=%23FFFFFF&icon=baidu.svg&icon_color=%23726161&title=%C2%B7&edge_flat=false"/></a>
   <br>
   <img src="https://github-readme-stats.vercel.app/api?username=busymidnight&show_icons=true">
@@ -39,9 +39,10 @@ markdown_text="""
   <br>
   </div>
  
-# :mailbox_with_mail: Contacts
+## :mailbox_with_mail: Contacts
 [![Tech Blog Badge](http://img.shields.io/badge/-Tech%20blog-black?style=flat-square&logo=github&link=https://un-lazy-midnight.tistory.com/)](https://soo-vely-dev.tistory.com/)
 [![Gmail Badge](https://img.shields.io/badge/Gmail-d14836?style=flat-square&logo=Gmail&logoColor=white&link=mailto:busymidnight17@gmail.com)](mailto:kimsh1691@gmail.com)
+
 ## 🪄Latest Blog Posts 🪄
 
   """
