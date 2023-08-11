040bcf1b3d299de9f9befc5edb7d18872636312f
YK Park <yangang4you@naver.com>
2023-08-12 03:11:08 +0900

    Update main.py

--- main.py
+++ main.py
@@ -29,9 +29,7 @@ markdown_text="""
    <br>
  </p>
   <br>
-  <a href="https://hits.seeyoufarm.com"><img src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2Fbusymidnight&count_bg=%23BEBEBE&title_bg=%23FFFFFF&icon=baidu.svg&icon_color=%23726161&title=%C2%B7&edge_flat=false"/></a>
-  <br>
-  <img src="https://github-readme-stats.vercel.app/api?username=busymidnight&show_icons=true">
+  <img src="https://github-readme-stats.vercel.app/api?username=busymidnight&show_icons=truetheme=dracula">
   <br>
   <br>
   </div>
